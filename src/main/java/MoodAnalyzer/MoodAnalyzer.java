1e42129c98f2853b021464db87334d8a26a7c8ea
Jay-R-H <jayhiremathr@gmail.com>
2021-10-21 12:51:37 +0530

    Initial commit

--- src/main/java/MoodAnalyzer/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer/MoodAnalyzer.java
@@ -2,17 +2,5 @@ package MoodAnalyzer;
 
 public class MoodAnalyzer {
 
-    private String message;
-
-    public MoodAnalyzer(String message) {
-        this.message = message;
     }
 
-    public String analyseMood(){
-        if (message.toLowerCase().equals("sad")){
-            return "SAD";
-        }
-        else
-            return "HAPPY";
-    }
-}
